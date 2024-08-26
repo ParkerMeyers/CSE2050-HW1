eb781b689fb0d07364607dcd51ffcbcf68bbe074
Parker Meyers <parker@parkermeyers.com>
2024-08-26 10:35:44 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# CSE 2050 - Mod 1 HW
+> **Python Fundamentals**
+> Due date: 9/3/2024 at 11:59PM
+> Author: Parker Meyers @ UCONN
+
+Reference [Instructions (click)](INSTRUCTIONS.md)
+
+## Objectives
+1.  **Use vocabulary**: local variables, functions, arguments, parameters, return values, mutable, immutable, module
+2.  Use **sequence**, **selection**, and **iteration** tools (if, while, and for) to solve problems with Python
+3.  **Use Python's standard atomic types**
+	-   ints
+	-   floats
+	-   bools
+	**& collections**
+	-   strings
+	-   lists
+	-   tuples
+	-   sets
+	-   dictionaries
+	**and choose the correct type for a given situation**
+	4.  Identify the types of variables in a Python program
+	5.  Write and evaluate comprehensions for Python collections (not just lists!)
+	6.  Write Python functions
+	7.  Write a complete Python program from scratch that includes imports, functions, conditionals, arithmetic, and loops
+
+## Resources
+- **Textbook**
+	- Chapter **#2**
---
+++ fizz_buzz.py
@@ -0,0 +1,16 @@
+###############################################################################
+# Collaboration                                                               #
+# -------------                                                               #
+# You can collaborate with up to 3 classmates (for a total of 4 students per  #
+# group). If you do so, remember not to share code directly. Discussions are  #
+# fine, code sharing is not. Also note that all have to submit individually.  #
+#                                                                             #
+# Enter any collaborators here:                                               #
+# Collaborator 1:                                                             #
+# Collaborator 2:                                                             #
+# Collaborator 3:                                                             #
+###############################################################################
+
+def fizz_buzz(start, finish):
+    # Delete this comment and below, then start working.
+    # Make sure you add a docstring to this function.
