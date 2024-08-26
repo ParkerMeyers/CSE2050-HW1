88e5558feab5dde2a0eb5707b5da35df35f79506
Parker Meyers <parker@parkermeyers.com>
2024-08-26 16:33:11 -0400

    Fixed fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -12,7 +12,7 @@ def fizz_buzz(start, finish):
     # Loop through numbers "start" to "finish"
     for i in range(start, finish + 1):
         # If the number is a multiple of 3 and 5
-        if i % 3 == 0 and i % 5 == 0:
+        if (i % 3 == 0 or "3" in str(i)) and (i % 5 == 0 or "5" in str(i)):
             print("fizzbuzz")
         # If the number is a multiple of 3 or contains 3
         elif i % 3 == 0 or "3" in str(i):
